6914c29f3cc7bceaed49ee664bf5ec8ea793cff0
romankap <romankap@gmail.com>
2016-07-05 13:00:34 +0300

    Fixed S-W on ReCAM bugs

--- Simulator/SW_on_ReCAM.py
+++ Simulator/SW_on_ReCAM.py
@@ -19,12 +19,12 @@ def getOperatingRows(i, offset, lenA, lenB):
     elif i < lenA:
         start_row = i-lenB+1; end_row = i
     else:
-        start_row = i-lenA+lenB-1; end_row = lenA-1
+        start_row = i-lenB+1; end_row = lenA-1
 
     return start_row+offset, end_row+offset
 
 def SW_on_ReCAM(input_seqA="AGCT", input_seqB="GCT"):
-    storage = ReCAM.ReCAM(512)
+    storage = ReCAM.ReCAM(2048)
     print("size in bytes = ", storage.sizeInBytes)
     print("bits per row = ", storage.bitsPerRow)
 
@@ -53,7 +53,7 @@ def SW_on_ReCAM(input_seqA="AGCT", input_seqB="GCT"):
     E_col_index = 2; F_col_index = 3; first_AD_col_index = 4; last_AD_col_index = 6; temp_col_index = 7
     total_max_score = 0; total_max_row_index = 0; total_max_col_index = 0
 
-    storage.setVerbose(True)
+    ##storage.setVerbose(True)
     storage.setPrintHeader(table_header_row)
 
     #for i in range (0, len(seqA)+len(seqB)+2):
@@ -65,31 +65,22 @@ def SW_on_ReCAM(input_seqA="AGCT", input_seqB="GCT"):
         left_AD = ((i-2) % 3) + first_AD_col_index
 
         storage.shiftColumn(seqB_col_index, i, len(seqB)+i-1, 1) # Prepare SeqB for match score
-
-        if i >= len(seqB):
-            storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
-        else:
-            storage.shiftColumn(left_AD, start_row, end_row, 1)
+        storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
 
         storage.DNAbpMatch(seqA_col_index, seqB_col_index, temp_col_index, start_row, end_row, DNA_match_score, DNA_mismatch_score)
         storage.rowWiseOperation(left_AD, temp_col_index, right_AD, start_row, end_row, '+')
         storage.rowWiseOperationWithConstant(right_AD, 0, right_AD, start_row, end_row, "max")
 
-        if i >= len(seqB):
-            storage.rowWiseOperationWithConstant(middle_AD, DNA_gap_first, left_AD, start_row-1, end_row, '+')
-        else:
-            storage.rowWiseOperationWithConstant(middle_AD, DNA_gap_first, left_AD, start_row, end_row, '+')
-
+        storage.rowWiseOperationWithConstant(middle_AD, DNA_gap_first, left_AD, start_row, end_row, '+')
         storage.rowWiseOperationWithConstant(F_col_index, DNA_gap_extend, temp_col_index, start_row, end_row, '+')
         storage.rowWiseOperation(left_AD, temp_col_index, F_col_index, start_row, end_row, "max")
         storage.rowWiseOperation(right_AD, F_col_index, right_AD, start_row, end_row, "max")
-        storage.rowWiseOperationWithConstant(E_col_index, DNA_gap_extend, temp_col_index, start_row, end_row, '+')
 
-        if i >= len(seqB):
-            storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
-        else:
-            storage.shiftColumn(left_AD, start_row, end_row, 1)
-        storage.rowWiseOperation(left_AD, temp_col_index, E_col_index, start_row, end_row, "max")
+        storage.shiftColumn(E_col_index, start_row-1, end_row-1, 1)
+        storage.rowWiseOperationWithConstant(E_col_index, DNA_gap_extend, E_col_index, start_row, end_row, '+')
+        storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
+        storage.rowWiseOperation(left_AD, E_col_index, E_col_index, start_row+1, end_row, "max")
+
         storage.rowWiseOperation(right_AD, E_col_index, right_AD, start_row, end_row, "max")
 
         (cycles, max_score_in_column, row_of_max_score_in_column) = storage.getScalarFromColumn(right_AD, start_row, end_row, "max")
@@ -103,6 +94,8 @@ def SW_on_ReCAM(input_seqA="AGCT", input_seqB="GCT"):
         print("\n\n\n")
         print("Max score = ", total_max_score, ", in (row,col)=(", total_max_row_index, ", ", total_max_col_index, ")")
         print("\n")
-        storage.printArray(header=table_header_row, tablefmt="grid")
+        #storage.printArray(header=table_header_row, tablefmt="grid")
 
-SW_on_ReCAM()
+    return (total_max_score, total_max_row_index, total_max_col_index)
+
+#SW_on_ReCAM()

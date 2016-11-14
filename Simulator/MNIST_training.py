1aaf62ec6186ba369b3870a5fff4b18fc63908a4
romankap <romankap@gmail.com>
2016-11-14 16:06:53 +0200

    MNIST_training: modify CPU calls after activations refactor

--- Simulator/MNIST_training.py
+++ Simulator/MNIST_training.py
@@ -30,7 +30,7 @@ def train_MNIST():
     nn_input_size = 784  # actual input length will be +1 due to bias
     fixed_point_10bit_precision = FixedPoint.FixedPointFormat(6, 10)
     nn = NeuralNetwork.createMNISTFullyConnectNN(fixed_point_10bit_precision, nn_input_size)
-    NN_on_CPU = NNs_on_CPU.initialize_NN_on_CPU(fixed_point_10bit_precision)
+    NN_on_CPU = NNs_on_CPU.initialize_NN_on_CPU(nn, fixed_point_10bit_precision)
 
     learning_rate = 0.02
     mini_batch_size = 50
@@ -41,7 +41,7 @@ def train_MNIST():
     # --- ReCAM ---#
     nn_weights_column = 0
     nn_start_row = 0
-    ReCAM_size = 4194304
+    ReCAM_size = 419430400
     NN_on_ReCAM = NNs_on_ReCAM.initialize_NN_on_ReCAM(nn_weights_column, nn_start_row, ReCAM_size)
     NN_on_ReCAM.set_SGD_parameters(mini_batch_size, learning_rate)
 
@@ -56,18 +56,17 @@ def train_MNIST():
             target_output[train_label] = 1
 
             #--- ReCAM ---#
-            NN_on_ReCAM.SGD_train(nn, fixed_point_10bit_precision, nn_input_size, train_image, target_output)
-            ReCAM_weights = NN_on_ReCAM.get_NN_matrices(nn, nn_weights_column, nn_start_row)
+            ###NN_on_ReCAM.SGD_train(nn, fixed_point_10bit_precision, nn_input_size, train_image, target_output)
+            ###ReCAM_weights = NN_on_ReCAM.get_NN_matrices(nn, nn_weights_column, nn_start_row)
             #print("Finished ReCAM Execution", training_iteration)
 
             #--- CPU ---#
-            #NN_on_CPU.SGD_train(nn, train_image, target_output)
-            #print("Finished CPU Execution", training_iteration)
+            NN_on_CPU.SGD_train(nn, train_image, target_output)
+            print("Finished CPU Execution", training_iteration)
 
             # --- Verify weights match ---#
             #NNs_unit_tests.compare_NN_matrices(ReCAM_weights, nn.weightsMatrices, "weights")
             aux_functions.write_to_output_file("Training iteration: ", training_iteration,
-                                               ". Target output:", target_output,
                                                ". Target output:", target_output)
 
         print("Training epoch: ", epoch_number)

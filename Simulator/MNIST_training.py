3297c72aa657a9a9720d47e1e5b96a28b583e8b9
romankap <romankap@gmail.com>
2017-04-25 15:49:33 +0300

    Activate CPU training

--- Simulator/MNIST_training.py
+++ Simulator/MNIST_training.py
@@ -31,13 +31,13 @@ def train_MNIST():
     fixed_point_10bit_precision = FixedPoint.FixedPointFormat(6, 10)
     nn = NeuralNetwork.createMNISTFullyConnectNN(fixed_point_10bit_precision, nn_input_size)
     ##nn = NeuralNetwork.createDemoFullyConnectNN(fixed_point_10bit_precision, nn_input_size)
-    #NN_on_CPU = NNs_on_CPU.initialize_NN_on_CPU(nn, fixed_point_10bit_precision)
+    NN_on_CPU = NNs_on_CPU.initialize_NN_on_CPU(nn, fixed_point_10bit_precision)
 
     learning_rate = 0.02
     mini_batch_size = 50
 
     # --- CPU ---#
-    #NN_on_CPU.set_SGD_parameters(nn, mini_batch_size, learning_rate)
+    NN_on_CPU.set_SGD_parameters(nn, mini_batch_size, learning_rate)
 
     # --- ReCAM ---#
     nn_weights_column = 0
@@ -63,30 +63,30 @@ def train_MNIST():
     #ReCAM_weights = NN_on_ReCAM.get_NN_matrices(nn, nn_weights_column, nn_start_row)
     NN_on_ReCAM.storage.printHistogramsToExcel(nn, len(mnist_data.train_images))
 
-        #print("Finished ReCAM Execution", training_iteration)
+    # print("Finished ReCAM Execution", training_iteration)
 
-        #--- CPU ---#
-        #NN_on_CPU.SGD_train(nn, train_image, target_output)
-        #print("Finished CPU Execution", training_iteration)
+    #--- CPU ---#
+    NN_on_CPU.SGD_train(nn, train_image, target_output)
+    print("Finished CPU Execution", training_iteration)
 
-        # --- Verify weights match ---#
-        #NNs_unit_tests.compare_NN_matrices(ReCAM_weights, nn.weightsMatrices, "weights")
-    '''        aux_functions.write_to_output_file("Training iteration: ", training_iteration,
-                                           ". Target output:", target_output)
+    # --- Verify weights match ---#
+    #NNs_unit_tests.compare_NN_matrices(ReCAM_weights, nn.weightsMatrices, "weights")
+'''        aux_functions.write_to_output_file("Training iteration: ", training_iteration,
+                                       ". Target output:", target_output)
 
-        print("Training epoch: ", epoch_number)
-        aux_functions.write_to_output_file("Training epoch: ", epoch_number)
+    print("Training epoch: ", epoch_number)
+    aux_functions.write_to_output_file("Training epoch: ", epoch_number)
 
-        number_of_correct_classifications = 0
-        for testing_iteration in range(len(mnist_data.test_images)):
-            test_image = mnist_data.test_images[testing_iteration]
-            ReCAM_FF_output = NN_on_ReCAM.get_feedforward_output(nn, fixed_point_10bit_precision, nn_input_size, test_image)
-            ReCAM_sample_label = get_MNIST_class_from_output(ReCAM_FF_output)
-            if ReCAM_sample_label == mnist_data.test_labels[testing_iteration]:
-                number_of_correct_classifications += 1
+    number_of_correct_classifications = 0
+    for testing_iteration in range(len(mnist_data.test_images)):
+        test_image = mnist_data.test_images[testing_iteration]
+        ReCAM_FF_output = NN_on_ReCAM.get_feedforward_output(nn, fixed_point_10bit_precision, nn_input_size, test_image)
+        ReCAM_sample_label = get_MNIST_class_from_output(ReCAM_FF_output)
+        if ReCAM_sample_label == mnist_data.test_labels[testing_iteration]:
+            number_of_correct_classifications += 1
 
-        percentage_of_correct_classifications = number_of_correct_classifications / len(mnist_data.test_images)
-        aux_functions.write_to_output_file("epoch number:", epoch_number, ". ReCAM percentage of correct classifications:", percentage_of_correct_classifications)
+    percentage_of_correct_classifications = number_of_correct_classifications / len(mnist_data.test_images)
+    aux_functions.write_to_output_file("epoch number:", epoch_number, ". ReCAM percentage of correct classifications:", percentage_of_correct_classifications)
 
     aux_functions.close_output_file()
 '''

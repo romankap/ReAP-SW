5b98063b55fb581584b3297b4704bd3283c8e288
romankap <romankap@gmail.com>
2017-06-29 13:32:57 +0300

    Modified algorithm to use tagged rows (still missing some functionality)

--- Simulator/Parallel_SW_on_ReCAM.py
+++ Simulator/Parallel_SW_on_ReCAM.py
@@ -1,11 +1,14 @@
 import os
 import sys
-
 import ReCAM
+import ProteinSequencing.protein_matrix_class as Protein_Matrix_Class
+import ProteinSequencing.BLOSUM62 as BLOSUM62
 
 lib_path = os.path.abspath(os.path.join('spfpm-1.1'))
 sys.path.append(lib_path)
 
+DNA_bp_bits = 2
+amino_acid_bits = 5
 DNA_match_score = 2; DNA_mismatch_score = -1; DNA_gap_first = -1; DNA_gap_extend = -1
 
 def test_func(test_param):
@@ -24,7 +27,7 @@ def getOperatingRows(iteration, offset, lenA, lenB):
     return start_row+offset, end_row+offset
 
 
-def place_DB_seq_in_ReCAM(storage, start_row, first_row_col, last_row_col, seq_col, seq_as_list):
+def place_DB_seq_in_ReCAM(storage, start_row, first_row_col, last_row_col, seq_col, DB_seq_column_bits, seq_as_list):
     # 1. set first_row bit
 
     first_last_row_vec = [0] * len(seq_as_list)
@@ -37,7 +40,7 @@ def place_DB_seq_in_ReCAM(storage, start_row, first_row_col, last_row_col, seq_c
     storage.loadData(first_last_row_vec, start_row, 1, last_row_col)
 
     # 3. load sequence
-    storage.loadData(seq_as_list, start_row, 5, seq_col)
+    storage.loadData(seq_as_list, start_row, DB_seq_column_bits, seq_col)
 
 
 def Parallel_SW_on_ReCAM(DB_sequences, query_seq):
@@ -48,74 +51,86 @@ def Parallel_SW_on_ReCAM(DB_sequences, query_seq):
         print("size in bytes = ", storage.sizeInBytes)
         print("bits per row = ", storage.bitsPerRow)
 
-    num_of_DB_seqs = len(DB_sequences)
-
-    seqA = list(DB_sequences[0])
-    seqB = list(DB_sequences[1])
     zero_vector = [0] * storage.rowsNum
 
-    table_header_row = ["first_row_bit", "last_row_bit", "seqA", "seqB", "E[]", "F[]", "AD[0]", "AD[1]", "AD[2]", "temp[]"]
+    table_header_row = ["first_row_bit", "last_row_bit", "active_row_bit", "seqA", "seqB", "E[]", "F[]", "AD[0]", "AD[1]", "AD[2]", "Max-AD-Scores", "temp[]"]
     storage.setVerbose(verbose_prints)
     storage.setPrintHeader(table_header_row)
 
     # Definitions
-    first_row_col = 0; last_row_col = 1; DB_seq_col = 2
-    E_col_index = 5; F_col_index = 6; first_AD_col_index = 7; last_AD_col_index = 8; temp_col_index = 9
+    first_row_col = 0; last_row_col = 1; active_bit_col = 2
+    DB_seq_col = 3; query_seq_col = 4
+    E_col_index = 5; F_col_index = 6; first_AD_col_index = 7; last_AD_col_index = 8; max_AD_scores_col_index = 9; temp_col_index = 10
     total_max_score = 0; total_max_row_index = 0; total_max_col_index = 0
 
-    for i in range(2):
+    for i in range(3):
         storage.loadData(zero_vector, 0, 1) # load first/last row columns
     for i in range(2):
         storage.loadData(zero_vector, 0, 5) # load protein columns
     for i in range(6):
         storage.loadData(zero_vector, 0, 32)  # load scores columns
 
-    start_row = 0
+    copy_start_row = 0; max_seq_len=0
     for seq in DB_sequences:
         seq_as_list = list(seq)
-        place_DB_seq_in_ReCAM(storage, start_row, first_row_col, last_row_col, DB_seq_col, seq_as_list)
-        start_row += len(seq)
+        place_DB_seq_in_ReCAM(storage, copy_start_row, first_row_col, last_row_col, DB_seq_col, amino_acid_bits, seq_as_list)
+        copy_start_row += len(seq)
+        if max_seq_len < len(seq_as_list):
+            max_seq_len = len(seq_as_list)
 
+    alg_end_row = copy_start_row
+    alg_start_row = 0
+    query_seq_len = len(query_seq)
 
+    ##### --- Algorithm --- #####
+
+    # Init
+    # 1) Copy first_row bit to active_bit
+    # 2) Copy first query_seq character
+
+    protein_matrix = Protein_Matrix_Class.blosum_matrix(BLOSUM62.full_blosum62())
+    ReCAM.set_match_matrix('protein', protein_matrix)
     #for i in range (0, len(seqA)+len(seqB)+2):
-    for i in range (0, len(seqA)+len(seqB)-1):
-        start_row, end_row = getOperatingRows(i, seqA_start_row, len(seqA), len(seqB))
+    for i in range(0, max_seq_len + query_seq_len - 1):
+        # Alg iteration outline:
+        # 1) Shift query seq down on all active rows
+        # 2) shift down active rows bit
+        # 3) Once right_AD score was calculated, max-copy it to a max-AD-scores column
+
+        # x) After the execution has finished, perform a max-reduction on max-AD-scores of all DB sequences
+
+        #start_row, end_row = getOperatingRows(i, seqA_start_row, len(seqA), len(seqB))
 
         right_AD = (i % 3) + first_AD_col_index
         middle_AD = ((i-1) % 3) + first_AD_col_index
         left_AD = ((i-2) % 3) + first_AD_col_index
 
-        storage.shiftColumn(seqB_col_index, i, len(seqB)+i-1, 1) # Prepare SeqB for match score
-        storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
+        if i==0:
+            tagged_rows_list = storage.tagRowsEqualToConstant(first_row_col, 1, alg_start_row, alg_end_row)
+            storage.taggedRowWiseOperation(first_row_col, None, active_bit_col, tagged_rows_list, 'copy')
+            storage.taggedRowWiseOperationWithConstant(None, query_seq[0], query_seq_col, tagged_rows_list, 'write')
+        else:
+            tagged_rows_list = storage.tagRowsEqualToConstant(active_bit_col, 1, alg_start_row, alg_end_row)
+            storage.shiftColumnOnTaggedRows(query_seq_col, tagged_rows_list)
+            storage.shiftColumnOnTaggedRows(left_AD, tagged_rows_list)
 
-        storage.DNAbpMatch(seqA_col_index, seqB_col_index, temp_col_index, start_row, end_row, DNA_match_score, DNA_mismatch_score)
-        storage.rowWiseOperation(left_AD, temp_col_index, right_AD, start_row, end_row, '+')
-        storage.rowWiseOperationWithConstant(right_AD, 0, right_AD, start_row, end_row, "max")
+        storage.SeqMatchOnTaggedRows(DB_seq_col, query_seq_col, temp_col_index, tagged_rows_list)
+        storage.taggedRowWiseOperation(left_AD, temp_col_index, right_AD, tagged_rows_list, '+')
+        storage.taggedRowWiseOperation(right_AD, 0, right_AD, tagged_rows_list, "max")
 
-        storage.rowWiseOperationWithConstant(middle_AD, DNA_gap_first, left_AD, start_row, end_row, '+')
-        storage.rowWiseOperationWithConstant(F_col_index, DNA_gap_extend, temp_col_index, start_row, end_row, '+')
-        storage.rowWiseOperation(left_AD, temp_col_index, F_col_index, start_row, end_row, "max")
-        storage.rowWiseOperation(right_AD, F_col_index, right_AD, start_row, end_row, "max")
+        storage.taggedRowWiseOperationWithConstant(middle_AD, DNA_gap_first, left_AD, tagged_rows_list, '+')
+        storage.taggedRowWiseOperationWithConstant(F_col_index, DNA_gap_extend, temp_col_index, tagged_rows_list, '+')
+        storage.taggedRowWiseOperation(left_AD, temp_col_index, F_col_index, tagged_rows_list, "max")
+        storage.taggedRowWiseOperation(right_AD, F_col_index, right_AD, tagged_rows_list, "max")
 
-        storage.shiftColumn(E_col_index, start_row-1, end_row-1, 1)
-        storage.rowWiseOperationWithConstant(E_col_index, DNA_gap_extend, E_col_index, start_row, end_row, '+')
+        storage.shiftColumn(E_col_index, start_row - 1, end_row - 1, 1)
+        storage.taggedRowWiseOperationWithConstant(E_col_index, DNA_gap_extend, E_col_index, tagged_rows_list, '+')
         storage.shiftColumn(left_AD, start_row-1, end_row-1, 1)
         storage.rowWiseOperation(left_AD, E_col_index, E_col_index, start_row+1, end_row, "max")
 
-        storage.rowWiseOperation(right_AD, E_col_index, right_AD, start_row, end_row, "max")
+        storage.taggedRowWiseOperation(right_AD, E_col_index, right_AD, tagged_rows_list, "max")
+        storage.taggedRowWiseOperation(right_AD, max_AD_scores_col_index, right_AD, tagged_rows_list, "max")
 
-        (max_score_in_column, row_of_max_score_in_column) = storage.getScalarFromColumn(right_AD, start_row, end_row, "max")
-        if max_score_in_column > total_max_score:
-            total_max_score = max_score_in_column
-            total_max_row_index = row_of_max_score_in_column-(seqA_start_row-1) #seqA starts from row 1
-            total_max_col_index = i-total_max_row_index+2
-
-        #time.sleep(1)
-        # os.system('cls')
-        # print("\n\n\n")
-        # print("Max score = ", total_max_score, ", in (row,col)=(", total_max_row_index, ", ", total_max_col_index, ")")
-        # print("\n")
-        #storage.printArray(header=table_header_row, tablefmt="grid")
 
     print("=== ReCAM Cycles executed: ", storage.getCyclesCounter())
     print("* SeqA length = ", len(seqA), " seqB length = ", len(seqB))
